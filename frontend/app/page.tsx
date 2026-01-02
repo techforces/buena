95e4f9422914ae8ac79f3cc4bce4db0a64401ec0
khurshid <ernazarovkh@gmail.com>
2026-01-02 14:18:40 +0100

    feat(frontend): init frontend

---
+++ frontend/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div>
+      <div className=" bg-black text-white">Hello world</div>
+    </div>
+  );
+}
---
+++ frontend/tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/components/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        blue: "#2A63F1",
+        night: "#1C1917",
+        grey: "#A6A09B",
+        "pale-100": "#F9F9F9",
+        "pale-200": "#F0F0F0",
+      },
+    },
+  },
+  plugins: [],
+};
